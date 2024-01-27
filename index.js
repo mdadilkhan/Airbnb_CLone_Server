9479364ba472ff328221c4d086db64162f3ef38e
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2024-01-27 21:13:49 +0530

    orfigin issue checking

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express()
 app.use(express.json())
 
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://airbnb-29671.web.app" }));
 app.use(cookieParser());
 
 
